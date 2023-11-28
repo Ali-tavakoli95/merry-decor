8a4da84b428122040a9ba54d145ff159001ce41e
Ali Tavakoli (iLiYa) <ali.tavakoli.95.at@gmail.com>
2023-11-28 14:55:46 +0330

    create website

---
+++ client/src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
