43e2ebb1ce0db2d5931cf581341742f569cdc66e
Ali Tavakoli (iLiYa) <ali.tavakoli.95.at@gmail.com>
2023-11-29 23:02:54 +0330

    create header & footer

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { BabyDecorComponent } from './components/baby-decor/baby-decor.component';
 import { WorkSamplesComponent } from './components/work-samples/work-samples.component';
 import { ChildrenCastleComponent } from './components/children-castle/children-castle.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -10,4 +11,5 @@ export const routes: Routes = [
     { path: 'work-samples', component: WorkSamplesComponent },
     { path: 'baby-decor', component: BabyDecorComponent },
     { path: 'children-castle', component: ChildrenCastleComponent },
+    {path: 'contact-us', component: ContactUsComponent}
 ];
